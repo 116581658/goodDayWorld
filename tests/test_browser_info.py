1f1b080bad66651b9bb6d7a8afa28726d52da7e2
116581658 <jankoyangue@yahoo.com>
2025-11-17 05:30:27 +0200

    Add first playwright tests

--- tests/test_browser_info.py
+++ tests/test_browser_info.py
@@ -2,5 +2,7 @@ def test_browser_name(playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+    page.goto("https://playwright.dev")
     print("Using browser: Chromium")
-    browser.close()
+    # browser.close()
+    input("Browser is running. Press ENTER to close...")
